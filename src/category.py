b9fcbd80db7c88e198ec40bbd0f8bdb686ac5453
maxim <mcsim55555k@gmail.com>
2026-04-25 20:19:30 +0300

    Homework_14_2 tasks done

--- src/category.py
+++ src/category.py
@@ -1,6 +1,5 @@
 from src.product import Product
 
-
 class Category:
     """Класс категорий товаров"""
 
@@ -14,7 +13,26 @@ class Category:
     def __init__(self, name: str, description: str, products: list[Product]):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
 
         Category.category_count += 1
-        Category.product_count += len(list(products))
+        Category.product_count += len(self.__products)
+
+
+    def add_product(self, product: Product):
+        Category.product_count += 1
+        return self.__products.append(product)
+
+    def get_products(self) -> list[Product]:
+        """Возвращает список продуктов в категории"""
+        return self.__products
+
+    @property
+    def products(self):
+        product_str = ''
+        for product in self.__products:
+            product_str += f'{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n'
+        return product_str
+
+
+
